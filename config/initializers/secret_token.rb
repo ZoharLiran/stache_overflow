36b45605396f55aa0fcb8d771100160974c2ec83
Drew Parker & Katherine Hayden <pair+drewcp87.katherineimogene@devbootcamp.com>
2014-04-03 16:20:30 -0700

    made me some rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StacheOverflow::Application.config.secret_key_base = 'ff19d879e21570c049bea7a4622ffd414dece6a108237cfa6a124882de0820c4f5c4a3d5983e179765c44d3c563a79632af687f360b10e436eb36eb976c69478'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StacheOverflow::Application.config.session_store :cookie_store, key: '_stache_overflow_session'
