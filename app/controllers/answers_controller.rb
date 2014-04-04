546980eded381f6dfba081e1308ffee7647b803a
Apprentice <apprentice@dbc30.local>
2014-04-03 17:25:54 -0700

    Add new and show routes and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,35 @@
 class AnswersController < ApplicationController
-  def edit
-  end
-
   def index
     @answers = Answer.all
   end
 
   def show
+    @answer = Answer.find(params[:id])
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
   end
 
   def new
+    @answer = Answer.new
   end
+
+  def create
+    @answer = Answer.new(params[:answer])
+    if @answer.save
+      redirect_to answer_path(@answer)
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
 end
