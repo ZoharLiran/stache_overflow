913a70ef145753a23153453905c4e9700453918a
Apprentice <apprentice@dbc30.local>
2014-04-03 18:36:57 -0700

    Added seeds for questions->answers->acomments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+6.times do
+  q = Question.create(content: "#{Faker::Lorem.sentence}?")
+  4.times do
+    a = Answer.create(content: "#{Faker::Commerce.product_name}!")
+    2.times do
+      a.acomments.create(content: Faker::Company.catch_phrase)
+    end
+    q.answers << a
+  end
+end
