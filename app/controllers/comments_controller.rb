fdd47feb3b0ee6c4f7983cb69a28caa56a8919eb
jluyke <jordanluyke@gmail.com>
2014-04-04 20:53:48 -0700

    moved acomments to comments folder, got comments and answers working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,25 @@
 class CommentsController < ApplicationController
-  def create
+  def new
+    # @comments = Comment.all
+    @comment = Comment.new
+  end
 
+  def create
+    question = Question.find(params[:comment][:question_id])
+    @comment = question.comments.create(params[:comment])
+    p params
+    p "-"*50
+    p @comment
+    p @comment.id
+    if @comment.save
+      redirect_to question
+    else
+      render :new
+    end
   end
 
   def destroy
 
   end
 
-  def new
-
-  end
 end
