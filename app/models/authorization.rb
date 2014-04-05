920979cefc1db8f6a39537cb40b302842f3b2ace
Zohar Liran <zoharliran@gmail.com>
2014-04-05 12:55:31 -0700

    configuration for omniauth complete

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,2 +1,10 @@
 class Authorization < ActiveRecord::Base
+  def self.find_from_hash(hash)
+    find_nu_provider_and_uid(hash['provider'], hash['uid'])
+  end
+
+  def self.create_from_hash(hash, user = nil)
+    user ||= User.create_from_hash!(hash)
+    Authorization.create(user: user, uid: hash['uid'], provider: hash['provider'])
+  end
 end
