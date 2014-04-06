ea55ddb4e95d2a2f98a044307f8c9973f88078aa
jluyke <jordanluyke@gmail.com>
2014-04-06 16:50:43 -0700

    tests the main routes in questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -11,12 +11,28 @@ describe QuestionsController do
     end
   end
 
-  context '#best_answer' do
-    it "stores the best answer's id" do
-      get :show, :id => test_question.id
+  context "#create" do
+    it "creates a new question" do
       expect {
-        put :update, :id => test_question.id, :best_answer_id => test_answer.id
-      }.to change { test_question.reload.best_answer_id }.to eq(test_answer.id)
+        post :create, :question => { :content => "hello" }
+      }.to change{ Question.count }.by(1)
+    end
+  end
+
+  context "#update" do
+    it "updates a question" do
+      update_str = "this is a test update"
+      put :update, :id => test_question, :question => { :content => update_str }
+      expect(Question.find(test_question.id).content).to eq(update_str)
+    end
+  end
+
+  context "#best_answer" do
+    it "stores the best answer's id" do
+      expect {
+        test_question.best_answer_id = test_answer.id
+        test_question.save!
+      }.to change { Question.find(test_question.id).best_answer_id }.to eq(test_answer.id)
     end
   end
 end
