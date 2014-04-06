68e94ca42914a4eec846f19986e20b12a1f5333b
Mason O'Neal <masonwid@gmail.com>
2014-04-06 12:50:57 -0700

    Add jquery and ajax for voting on questions

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,17 +1,51 @@
 
 $(document).ready(function() {
 
-
 // below is draft
   $(".upvote-button").on("click", function() {
     var id = $(this).data('id')
+
+    // mason will test the 3 lines below
+    var voteString = $( ".v#{id}").text()
+    var newVoteInt = parseInt(voteString) + 1
+    var newVoteString = newVoteInt.toString()
+
     console.log(id)
-    // $.ajax
 
-    // "ajax:success", function(e, data, status, xhr) {
+    $.ajax({
+      url: // need URL  /question/increase_vote
+      type: GET
+      data: id
+      // I am assuming dataType: is not needed
+    }).done (function() {
+    $(".v#{id}").html( newVoteString );
+    });
 
-
-    // $("body").append(data.author.name);
+    // update the vote number here
+    // example $("body").append(data.author.name);
   });
+
+  $(".upvote-button").on("click", function() {
+    var id = $(this).data('id')
+
+    var voteString = $( ".v#{id}").text()
+    var newVoteInt = parseInt(voteString) - 1
+    var newVoteString = newVoteInt.toString()
+
+    console.log(id)
+    $.ajax({
+      url: // need URL  /question/decrease_vote
+      type: GET
+      data: id
+    }).done (function() {
+    $(".v#{id}").html( newVoteString );
+    });
+
+    // update the vote number here
+    // example $("body").append(data.author.name);
+  });
+
+
+
 });
 
