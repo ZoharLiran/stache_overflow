d4a0dc114ab42e35bd9ed63228ffae9577f68143
Mason O'Neal <masonwid@gmail.com>
2014-04-06 11:28:43 -0700

    Initial jquery and ajax work

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,20 @@
+// All this logic will automatically be available in application.js.
+
+// modeled on Steve's ajax demo code
+
+$(document).ready(function() {
+
+
+// below is draft
+  $(".upvote-button").on("click", function() {
+    var id = $(this).data('id')
+    console.log(id)
+    // $.ajax
+
+    // "ajax:success", function(e, data, status, xhr) {
+
+
+    // $("body").append(data.author.name);
+  });
+});
+
