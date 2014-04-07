759db6e4a0015ef9ff6f809779493ddeef028134
jluyke <jordanluyke@gmail.com>
2014-04-06 20:04:52 -0700

    added create and delete tests for answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,43 +1,24 @@
 require 'spec_helper'
 
 describe AnswersController do
-
-  # describe "#index" do
-  #   let!(:myquestion){ FactoryGirl.create :question }
-  #   it "shows all the answers" do
-  #     get :index, :question_id => myquestion.id
-  #     expect(response).to be_ok
-  #   end
-  # end
-
-  # describe "#show" do
-  #   let!(:myanswer){ FactoryGirl.create :answer }
-  #   it "assigns @answer based on the answer param" do
-  #     get :show, :id => myanswer.id, :question_id => myanswer.question_id
-  #     expect(assigns(:answer)).to eq myanswer
-  #   end
-  # end
+  let!(:test_question) { FactoryGirl.create :question }
+  let!(:test_answer) { FactoryGirl.create :answer }
 
   context "#create" do
-    # let!(:myanswer){ FactoryGirl.create :answer }
-
     it "creates a new answer" do
-      # expect {
-      # p "-"*50
-        # p Answer.all.count
-        # post :create, :answer => { :content => "hello" }
-        # p Answer.all.count
-      # # }.to change{ Answer.all.count }.by(1)
+      expect {
+        post :create, :question_id => test_question, :answer => test_answer.attributes
+      }.to change{ Answer.count }.by(1)
     end
   end
 
-  # describe "#update" do
-  #   let(!:myanswer){ FactoryGirl.create :answer }
-  #   it "updates an existing answer" do
-  #     expect {
-  #       put :update, :id => myanswer.id,
-  #                    :answer => { :content => 'Change is good' }
-  #     }.to change { myanswer.reload.content }.to('Change is good')
-  #   end
-  # end
+  context "#delete" do
+    it "deletes an answer" do
+      question_delete = FactoryGirl.create :question
+      answer_delete = FactoryGirl.create :answer
+      expect {
+        delete :destroy, :id => answer_delete, :question_id => question_delete
+      }.to change { Answer.count }.by(-1)
+    end
+  end
 end
