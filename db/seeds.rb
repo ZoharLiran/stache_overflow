5522559c0b8252fe7fdb1a42dbc67aae69db5a96
jluyke <jordanluyke@gmail.com>
2014-04-06 21:24:50 -0700

    fixed from failing after dropping db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Question.destroy_all
 Answer.destroy_all
 Comment.destroy_all
 
+User.create(name: "Test", email: "test@test.com", phone: "999-888-7654")
 34.times do |i|
   u = User.all.sample
   q = u.questions.create(title: "Question #{i}", content:Faker::Lorem.sentence(3) + " ?")
